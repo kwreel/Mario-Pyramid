77b20a64bc33258f9189faf352940724801ab850
kwreel <113304064+kwreel@users.noreply.github.com>
2023-01-29 23:52:08 +0000

    render pyramid to the DOM instead of the console

--- mario.js
+++ mario.js
@@ -1,13 +1,17 @@
 let inputHeight = parseInt(prompt("What height of Pyramid do you want?"));
 printPyramid(inputHeight);
 function printPyramid(height) {
-  space = " ";
+  space = ".";
   brick = "#";
     for (i=1; i<=height; i++){
       row = "";
       spaces = height - i;
       row = row + space.repeat(spaces);
       row = row + brick.repeat(i) + "#";
-      console.log(row);
+      const para = document.createElement("p");
+      const node = document.createTextNode(row);
+      para.appendChild(node);
+      const element = document.getElementById("pyramid");
+      element.appendChild(para);
     }
 }
