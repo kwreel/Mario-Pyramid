8f0f350cef435232de4a5702992069f3cb33cde8
kwreel <113304064+kwreel@users.noreply.github.com>
2023-02-14 17:39:46 +0000

    Added slider and select input. styled page

--- mario.js
+++ mario.js
@@ -1,3 +1,4 @@
+changeBrickSymbol();
 function determineHeightAndThenDrawPyramid(){
   let x = document.getElementById("height").value;
   printPyramid(x);
@@ -5,16 +6,21 @@ function determineHeightAndThenDrawPyramid(){
 function printPyramid(height) {
   document.getElementById("pyramid").innerHTML = "";
   space = ".";
-  brick = "#";
+  //brick = brickSymbol;
     for (i=1; i<=height; i++){
       row = "";
       spaces = height - i;
       row = row + space.repeat(spaces);
-      row = row + brick.repeat(i) + "#";
+      row = row + brick.repeat(i) + brick;
       const para = document.createElement("p");
       const node = document.createTextNode(row);
       para.appendChild(node);
       const element = document.getElementById("pyramid");
       element.appendChild(para);
     }
+}
+function changeBrickSymbol(){
+  let y = document.getElementById("bricks");
+  brick = y.value;
+  determineHeightAndThenDrawPyramid();
 }
